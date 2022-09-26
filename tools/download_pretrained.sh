f39a4a0752fabc1eb81011b0433af69a6e9ff58c
Haotian (Ken) Tang <kentang@mit.edu>
2022-09-26 18:24:38 -0400

    Add training details (#150)
    * [Major] Update training details.
    
    * [Minor] Update README.md.
    
    * [Minor] Remove comment.

--- tools/download_pretrained.sh
+++ tools/download_pretrained.sh
@@ -1,9 +1,10 @@
 mkdir pretrained &&
 cd pretrained &&
-wget https://bevfusion.mit.edu/files/pretrained/bevfusion-det.pth &&
-wget https://bevfusion.mit.edu/files/pretrained/bevfusion-seg.pth &&
+wget https://bevfusion.mit.edu/files/pretrained_updated/bevfusion-det.pth &&
+wget https://bevfusion.mit.edu/files/pretrained_updated/bevfusion-seg.pth &&
 wget https://bevfusion.mit.edu/files/pretrained/lidar-only-det.pth &&
 wget https://bevfusion.mit.edu/files/pretrained/lidar-only-seg.pth &&
-wget https://bevfusion.mit.edu/files/pretrained/camera-only-det.pth &&
-wget https://bevfusion.mit.edu/files/pretrained/camera-only-seg.pth 
+wget https://bevfusion.mit.edu/files/pretrained_updated/camera-only-det.pth &&
+wget https://bevfusion.mit.edu/files/pretrained_updated/camera-only-seg.pth &&
+wget https://bevfusion.mit.edu/files/pretrained_updated/swint-nuimages-pretrained.pth
 
