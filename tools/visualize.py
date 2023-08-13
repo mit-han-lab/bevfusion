87d2ae20f920867c5c95d77708038aa4c596afb6
JeongMin-98 <jeongmin981@gmail.com>
2023-08-13 07:50:57 +0000

    [FIX] Cannot import torchpack.utils.tqdm

--- tools/visualize.py
+++ tools/visualize.py
@@ -10,7 +10,9 @@ from mmcv.parallel import MMDistributedDataParallel
 from mmcv.runner import load_checkpoint
 from torchpack import distributed as dist
 from torchpack.utils.config import configs
-from torchpack.utils.tqdm import tqdm
+# from torchpack.utils.tqdm import tqdm
+# Can not import torchpack.utils.tqdm so directly import tqdm
+from tqdm import tqdm
 
 from mmdet3d.core import LiDARInstance3DBoxes
 from mmdet3d.core.utils import visualize_camera, visualize_lidar, visualize_map
@@ -47,10 +49,9 @@ def main() -> None:
     parser.add_argument("--bbox-score", type=float, default=None)
     parser.add_argument("--map-score", type=float, default=0.5)
     parser.add_argument("--out-dir", type=str, default="viz")
-    args, opts = parser.parse_known_args()
+    args = parser.parse_args()
 
     configs.load(args.config, recursive=True)
-    configs.update(opts)
 
     cfg = Config(recursive_eval(configs), filename=args.config)
 
